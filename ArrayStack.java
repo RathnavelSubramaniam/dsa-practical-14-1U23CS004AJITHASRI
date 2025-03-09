6a61305dc5a23a45b7159afadfa76d5ae8ece0eb
1U23CS004AJITHASRI <139536264+1U23CS004AJITHASRI@users.noreply.github.com>
2025-03-09 11:58:52 +0000

    sub 14

--- ArrayStack.java
+++ ArrayStack.java
@@ -1,87 +1,77 @@
 import java.util.*;
+class ArrayStack{	
+public char[] stack;
+public int capacity;
+protected int top = -1;
+public ArrayStack(int len) {
+    capacity=len;
+stack = new char[len]; 
+}
+public int size()
+{
+    return (top+1);
+}
+public void push(char data) 
+{
+if (isFull())
+System.out.println("Stack is Overflow. Not possible to insert in Full stack");
+else
+stack[++top] = data;
+}
+public void pop() 
+{
+char data='0';
+if (isEmpty())
+{
+System.out.println("Stack is Underflow. No elements to be popped in Empty Stack");
 
-class ArrayStack {    
-    public char[] stack;
-    public int capacity;
-    protected int top = -1;
+}
+else
+{
+data = stack[top];
+top--;
+}
+System.out.print(data);
+}
+public boolean isFull()
+{
+    return (size()==capacity);
+}
+public boolean isEmpty()
+{
+    return (top<0);
+}
 
+public void display()
+{
+    if(isEmpty())
+    System.out.println("Stack is Underflow. No elements to display in Empty Stack");
+    else
+    {
+     System.out.println("Stack Elements");
+    for(int i=top;i>=0;i--)
+    System.out.println(stack[i]);
+    }
+}
+
+public static void main(String[] args)
+{
     
-    public ArrayStack(int len) {
-        capacity = len;
-        stack = new char[len];
+    Scanner sc=new Scanner(System.in);
+    System.out.println("Enter the string to reverse:"); 
+    String str=sc.nextLine();
+      ArrayStack arrstack=new ArrayStack(str.length());
+for(int i=0;i<str.length();i++)
+    {
+arrstack.push(str.charAt(i));
+    }
+System.out.println("Element is inserted in Stack using push.");
+arrstack.display();
+System.out.println("Reversed string using pop:");
+for(int i=str.length()-1;i>=0;i--)
+    {
+arrstack.pop();
     }
 
-    
-    public int size() {
-        return (top + 1);
-    }
-
-    
-    public void push(char data) {
-        if (isFull()) {
-            System.out.println("Stack Overflow. Cannot insert into full stack.");
-        } else {
-            stack[++top] = data;
-        }
-    }
-
-    
-    public char pop() {
-        if (isEmpty()) {
-            System.out.println("Stack Underflow. No elements to pop from empty stack.");
-            return '\0';  // Return null character if the stack is empty
-        } else {
-            char data = stack[top];
-            top--;
-            return data;
-        }
-    }
-
-    
-    public boolean isFull() {
-        return (size() == capacity);
-    }
-
-    
-    public boolean isEmpty() {
-        return (top < 0);
-    }
-
-    
-    public void display() {
-        if (isEmpty()) {
-            System.out.println("Stack Underflow. No elements to display in empty stack.");
-        } else {
-            System.out.println("Stack elements:");
-            for (int i = top; i >= 0; i--) {
-                System.out.println(stack[i]);
-            }
-        }
-    }
-
-    
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the string to reverse:");
-
-        
-        String str = sc.nextLine();
-        ArrayStack arrStack = new ArrayStack(str.length());
-
-        
-        for (int i = 0; i < str.length(); i++) {
-            arrStack.push(str.charAt(i));
-        }
-
-        System.out.println("Elements are inserted into the stack using push.");
-        arrStack.display();
-
-        
-        System.out.println("Reversed string using pop:");
-        StringBuilder reversedStr = new StringBuilder();
-        while (!arrStack.isEmpty()) {
-            reversedStr.append(arrStack.pop());
-        }
-        System.out.println(reversedStr.toString());
-    }
+}
 }
